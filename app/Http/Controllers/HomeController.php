7e8cf011256235abc34c10e5da9eba4af08b130a
jojo <lucky820107@gmail.com>
2025-05-13 14:54:56 +0800

    新增區塊樣板

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,4 +12,9 @@ class HomeController extends Controller
 
         return view('home');
     }
+
+    public function about(){
+
+        return view('about');
+    }
 }
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1>Hello World</h1>
+<h1>About</h1>
 
 @endsection
 
