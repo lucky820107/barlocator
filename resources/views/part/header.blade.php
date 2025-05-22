86e32fa0bd172c9b4c774bf613017ae9ef9ab41e
jojo <lucky820107@gmail.com>
2025-05-22 18:14:26 +0800

    header 改成blade

--- resources/views/part/header.blade.php
+++ resources/views/part/header.blade.php
@@ -1,3 +1,8 @@
 <header class="container">
-    <a href="/">{{ config('website.siteName')}}</a>
+    {{-- <a href="/">{{ config('website.siteName')}}</a> --}}
+    <header>
+        <a href="#">
+            <img src="" alt="logo">
+        </a>
+    </header>
 </header>
